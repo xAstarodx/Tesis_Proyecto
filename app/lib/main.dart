642d1b9ec79c8031f93987c092247095001af13c
Cirrus CI <support@cirruslabs.org>
2026-03-01 22:26:40 +0000

    funciona

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -7,7 +7,8 @@ void main() async {
 
   await Supabase.initialize(
     url: 'https://qimcbnumzypyxhnvimjt.supabase.co',
-    anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFpbWNibnVtenlweXhobnZpbWp0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzEwOTIxMjQsImV4cCI6MjA4NjY2ODEyNH0.qaYnTLRkLRpXjkuS194zjBz51IIVYFCa6KuRqJOEBxw',
+    anonKey:
+        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFpbWNibnVtenlweXhobnZpbWp0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzEwOTIxMjQsImV4cCI6MjA4NjY2ODEyNH0.qaYnTLRkLRpXjkuS194zjBz51IIVYFCa6KuRqJOEBxw',
   );
 
   runApp(const MyApp());
